4308b09ec1eee00e62ca4e10bdbf3118c5e88ba3
fadi-arto <117361779+fadi-arto@users.noreply.github.com>
2022-11-22 10:49:11 +0200

    ss
    ss

--- routes/clint.js
+++ routes/clint.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const {newclint , deletclint , Updateclint} = require('../controller/clint');
+const passport = require('passport');
 
 var router  = express.Router(); 
 
 router.post('/' , newclint);
 router.delete('/deletcl/:id',deletclint);
 router.put('/deletcl/:id', Updateclint);
+
+
+
+router.post('/login', passport.authenticate('local-signin', {
+    successRedirect: '/',
+   failureRedirect: '/logins',
+    failureFlash: true,
+  }))
+
+
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'shipping-jp' });
 });
+
+
+router.post('/login', passport.authenticate('local-signin', {
+  successRedirect: '/',
+ failureRedirect: '/logins',
+  failureFlash: true,
+}))
+
+
 
 module.exports = router;
