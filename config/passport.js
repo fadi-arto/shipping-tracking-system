42a4e806399d589166852aa81f6a0b7c549bf5d7
fadi-arto <117361779+fadi-arto@users.noreply.github.com>
2022-11-22 10:25:54 +0200

    ss
    ss

---
+++ config/passport.js
@@ -0,0 +1,32 @@
+const use = require('passport');
+const passport = require('passport');
+const { Strategy } = require(passport-local);
+const localStrategy =require('passport-local').Strategy;
+const clients = require('../model/clients');
+
+
+
+
+passport.use('local-signin', new localStrategy({
+    usernameField  : 'email',
+    passwordField : 'password',
+    passReqToCallback :true,
+    
+    } , (req , email , password , done)=>{
+        clients.findOne({email : email} , (err,clients)=>{
+            if(err){
+                return done(err)
+            }
+            if(! clients){
+                return done(null , false , req.flash('signinError','this clients not found'))
+            }
+            if(! clients.comparePassword(password)){
+                return done(null, false , req.flash('signinError','woring password'))
+            }
+            return done(null,clients)
+    
+        })
+    
+    
+    }))
+
